1c7c589baa2258d1fc9a31c5f6232ddc61b5ad77
ChristinaKaliora <develop@pirron.one>
2020-02-14 17:52:16 +0100

    all get requests done

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -4,6 +4,7 @@ const Projects = require('../data/helpers/projectModel.js');
 
 const router = express.Router();
 
+// GET projects
 router.get("/", (req, res) => {
     Projects.get()
     .then(projects => {
@@ -15,4 +16,31 @@ router.get("/", (req, res) => {
     });
 });
 
+
+//GET projects by id
+router.get('/:id', (req,res) => {
+    const id = req.params.id;
+
+    Projects.get(id)
+    .then(project => {
+        res.status(200).json(project);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({errorMessage: "Error getting this project."});
+    })
+})
+
+//GET all action for a specific project
+router.get('/:id/actions', (req,res) => {
+    const id = req.params.id;
+    Projects.getProjectActions(id)
+    .then(action => {
+        res.status(201).json(action);
+    })
+    .catch( error => {
+        console.log(error);
+        res.status(404).json({ errorMessage: 'This project doesnt exist'});
+    })
+})
 module.exports = router;
